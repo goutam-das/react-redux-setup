1ca0148634df1043d66665e884b382733565cb6c
matainja <utpal@matainja.com>
2020-05-03 15:30:20 +0530

    HRM implement

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const Index = () => <h1>Index Page</h1>;
-ReactDOM.render(<Index />, document.getElementById('root'))
+ReactDOM.render(<Index />, document.getElementById('root'));
+
+if (module.hot) {
+    module.hot.accept();
+}
